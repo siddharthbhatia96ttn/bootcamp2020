6a21a786a62a877136d714af88a77f0aa651bae3
siddharthbhatia96ttn <60692085+siddharthbhatia96ttn@users.noreply.github.com>
2020-03-22 23:29:15 +0530

    Delete tight_coupling.java

--- q-1/tight_coupling.java
+++
@@ -1,7 +0,0 @@
-package tight_coupling;
-public class tight_coupling {
-    public static void main(String[] args) {
-        traveller_tight t=new traveller_tight();
-        t.start_journey();
-    }
-}
